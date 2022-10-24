bea61df4cb23bdf3c652fe6826e19b92f605cb7e
klschoef <ks@itec.aau.at>
2022-10-24 13:38:09 +0200

    examples of Oct 24, 2022

--- src/com/klausschoeffmann/Schleifen/Break.java
+++ src/com/klausschoeffmann/Schleifen/Break.java
@@ -3,6 +3,10 @@ package com.klausschoeffmann.Schleifen;
 public class Break {
     public static void main(String[] args) {
 
+        /* dieser Code zeigt ein schlechtes Beispiel
+           für die Verwendung von breaks, da man hier
+           mehrere Ausstiegspunkte aus der Schleife hat!
+         */
         for (int i=10; i <= 100; i+=10) {
             if (i == 30)
                 break;
